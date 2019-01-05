f973956ecd6917ccfcf036dbffe184de292e9da9
Ron Railsmith <railsmith4@outlook.com>
2019-01-04 22:59:16 -0800

    more

--- test/system/visit_links_test.rb
+++ test/system/visit_links_test.rb
@@ -8,7 +8,13 @@ require "application_system_test_case"
 class VisitLinksTest < ApplicationSystemTestCase
   test "visiting links" do
     visit '/'
+    visit '/about'
+    visit '/blog'
+    visit '/contact'
+    visit '/home/index'
+    visit '/home/links'
+    visit '/'
     sleep 5
-    assert_selector "h1", text: "Home"
+    assert_selector "h1", text: "rails4.us"
   end
 end
