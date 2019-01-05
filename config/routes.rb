d7a3bebf427e710fce3a0350e9a71d9394aa0a13
Ron Railsmith <railsmith4@outlook.com>
2019-01-04 22:13:16 -0800

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'lessons/index'
+  get  'home/about'
+  get  'home/blog'
+  get  'home/contact'
+  get  'home/index'
+  root 'home#index'
+  get  '/about',   to: 'home#about'
+  get  '/blog',    to: 'home#blog'
+  get  '/contact', to: 'home#contact'
+  get ':controller(/:action)'  
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
 end
