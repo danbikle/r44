3aac541ec19ff10bc140ff98799d82e2969a3ed0
Ron Railsmith <railsmith4@outlook.com>
2019-01-07 00:20:03 -0800

    more

--- test/system/visit_links_test.rb
+++ test/system/visit_links_test.rb
@@ -13,8 +13,26 @@ class VisitLinksTest < ApplicationSystemTestCase
     visit '/contact'
     visit '/home/index'
     visit '/home/links'
+    visit '/lessons/index'
+    visit '/lessons/lesson001'
+    visit '/lessons/lesson002'
+    visit '/lessons/lesson003'
+    visit '/lessons/lesson004'
+    visit '/lessons/lesson005'
+    visit '/lessons/lesson006'
+    visit '/lessons/lesson007'
+    visit '/lessons/lesson008'
+    visit '/lessons/lesson009'
+    visit '/lessons/lesson010'
+    visit '/lessons/lesson011'
+    visit '/lessons/lesson012'
+    visit '/lessons/lesson013'
+    visit '/lessons/lesson014'
+    visit '/lessons/lesson015'
+    visit '/lessons/lesson016'
+    visit '/lessons/lesson017'
     visit '/'
     sleep 5
-    assert_selector "h1", text: "rails4.us"
+    assert_selector "h1", text: "rails4.us"    
   end
 end
